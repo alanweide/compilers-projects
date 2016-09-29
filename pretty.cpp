68f3bb25a64b0b96f26b70d25790925ff11148fd
alanweide <alan.d.weide@gmail.com>
2016-09-28 22:35:15 -0400

    beginning function printing - debugging

--- pretty.cpp
+++ pretty.cpp
@@ -51,8 +51,8 @@ void examineFunctionDeclaration(SgFunctionDeclaration* decl) {
   SgFunctionDefinition* def = decl->get_definition();
   if (def) {
     SgFunctionDeclaration* f_decl = def->get_declaration();
-    cout << "[debug: f_decl = " << f_decl->unparseToString() << "]" << endl;
-    cout << f_decl->get_orig_return_type()->class_name().getString() << " " << f_decl->get_name().getString() << "()";
+    cout << "[debug: f_decl = " << f_decl->get_orig_return_type()->unparseToString() << "]" << endl;
+    cout << f_decl->get_orig_return_type()->class_name() << " " << f_decl->get_name().getString() << "()";
 
     // TODO: parameter list
 
