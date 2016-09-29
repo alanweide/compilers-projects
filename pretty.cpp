e367200e18225154c1be1c70e67630720e911456
alanweide <alan.d.weide@gmail.com>
2016-09-29 00:13:41 -0400

    beginning statement printing

--- pretty.cpp
+++ pretty.cpp
@@ -194,12 +194,13 @@ void examineExpression(SgExpression* expr) {
       cout << printOperatorForBinaryOp(bi_expr);
       examineExpression(bi_expr->get_rhs_operand());
     }
-    case V_SgIntVal:
-    case V_SgLongIntVal:
-    case V_SgFloatVal:
-    case V_SgDoubleVal: {
+    case V_SgValueExp: {
       SgValueExp* val_exp = isSgValueExp(expr);
       cout << printValueExp(val_exp);
+    }
+    case V_SgAssignInitializer: {
+      SgAssignInitializer* init_expr = isSgAssignInitializer(expr);
+      examineExpression(init_expr->get_operand());
     }
     default:
       cout << "[UNHANDLED examineExpression (" << expr->class_name() << ")] " << expr->unparseToString();
