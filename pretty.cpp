9f5b20bbfe4c00ab464345242cc4f3f44f89f972
alanweide <alan.d.weide@gmail.com>
2016-09-30 00:11:08 -0400

    modified modfft2 to include while statement and if-else statment

--- pretty.cpp
+++ pretty.cpp
@@ -1,4 +1,19 @@
-#include "pretty.h"
+#include <rose.h>
+using namespace std;
+
+string printExpression(SgExpression* expr);
+string printType(SgType* type);
+string printOperatorForBinaryOp(SgBinaryOp* op);
+string printBinaryOp(SgBinaryOp* expr);
+string printStatement(SgStatement* stmt);
+string printForStmt(SgForStatement* for_stmt);
+string printWhileStmt(SgWhileStmt* while_stmt);
+string printDoWhileStmt(SgDoWhileStmt* dow_stmt);
+string printIfStmt(SgIfStmt* stmt);
+string printBasicBlock(SgBasicBlock* block);
+string printVariableDeclaration(SgVariableDeclaration* decl);
+string printFunctionDeclaration(SgFunctionDeclaration* decl);
+string prettyPrint(SgProject* project);
 
 string printExpression(SgExpression* expr) {
   string output = "";
