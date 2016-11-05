ea83c964eb19c335988b526195aa1d50c9659d18
alanweide <alan.d.weide@gmail.com>
2016-11-05 11:50:11 -0400

    Failed tests from project 4

---
+++ p4fail1.c
@@ -0,0 +1,7 @@
+int zz;
+int main() {
+	int a = 1;
+	a = - 1 + - a;
+	a = (a-11)*3;
+	return a;
+}
---
+++ p4fail2.c
@@ -0,0 +1,9 @@
+int zz;
+int main() {
+	int a = 1;
+	a = a ^ 4;
+	a = a & 7;
+	a = 1 | 2;
+	a = ~a;
+	return a;
+}
