0be9407ae06400526eb6291d0a6547e3c6b23855
alanweide <alan.d.weide@gmail.com>
2016-11-22 12:33:33 -0500

    Changed jtestin

--- fftmain.c
+++ fftmain.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include "originalfftdriver.c"
+// #include "originalfftdriver.c"
 #include "compiledfftdriver.c"
 
 int main() {
-	printf("original: %lf\n", originalfft());
+	// printf("original: %lf\n", originalfft());
 	printf("compiled: %lf\n", compiledfft());
 }
--- testing.c
+++ testing.c
@@ -5,10 +5,8 @@ int main() {
 	y = x + 73;
 	int i,j;
 	int z = 2;
-	for (i = 0; i < x; i++) {
-		for (j = 0; j < y; j++) {
-			z *= -1;
-		}
+	if (x < y) {
+		x = x + y;
 	}
 	return z;
 }
