d5049a3a5836cb637146b7671c231918fc16b01c
alanweide <alan.d.weide@gmail.com>
2016-11-21 21:30:12 -0500

    Fix for nested for loops

--- intermediate2.cpp
+++ intermediate2.cpp
@@ -769,7 +769,7 @@ StatementNode translatedForStmt(SgForStatement* for_stmt, string next) {
 
   s.code = begin + ": ;\n" + test_expr.code + "if (" + test_expr.addr + ") goto " + s.trueLabel + ";\n";
   s.code = s.code + "goto " + s.next + ";\n";
-  s.code = s.code + s.trueLabel + ": " + body_stmt.code;
+  s.code = s.code + s.trueLabel + ": ;\n" + body_stmt.code;
   s.code = s.code + incr_expr.code + "goto " + begin + ";\n";
   s.code = s.code + next + ": ;\n";
 
